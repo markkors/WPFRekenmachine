c6f9ce4d8914e3991254fbce0e10e3a26f74bd92
mark <mark.kors@gmail.com>
2020-12-08 22:52:40 +0100

    nCalc added SQRT / PERC added

--- CSharpWPFCalculator/ViewModel/ViewModelCalculator.cs
+++ CSharpWPFCalculator/ViewModel/ViewModelCalculator.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+
 
 namespace CSharpWPFCalculator.ViewModel {
     class ViewModelCalculator : INotifyPropertyChanged  {
@@ -14,14 +16,17 @@ namespace CSharpWPFCalculator.ViewModel {
         {
             NumberButtonCmd = new RelayCommand(new Action<object>(NumberButtonPressed));
             EqualButtonCmd = new RelayCommand(new Action<object>(EqualButtonPressed));
+            SpecialOperatorButtonCmd = new RelayCommand(new Action<object>(SpecialOperatorButtonPressed));
         }
+
+    
 
         private ICommand m_ButtonCommand1;
         private ICommand m_ButtonCommand2;
+        private ICommand m_ButtonCommand3;
         private StringBuilder _SBCalculation = new StringBuilder();
         private string _Calculation;
-        private Boolean doReset = false;
-
+       
         public ICommand NumberButtonCmd
         {
             get
@@ -46,38 +51,66 @@ namespace CSharpWPFCalculator.ViewModel {
             }
         }
 
+        public ICommand SpecialOperatorButtonCmd {
+            get { 
+                return m_ButtonCommand3; 
+            }
+            set { 
+                m_ButtonCommand3 = value;
+            } 
+        }
+
         public void NumberButtonPressed(object obj)
         {
-            if (doReset)
-            {
-                _SBCalculation.Clear();
-                doReset = false;
-            }
-
             _SBCalculation.Append(obj.ToString());
             Calculation = _SBCalculation.ToString();
         }
 
         public void EqualButtonPressed(object obj)
         {
+            doCalculate();
+        }
+
+        private void SpecialOperatorButtonPressed(object obj)
+        {
+            
+            if (obj.ToString() == "SQRT") {
+                Calculation = $"Sqrt({_SBCalculation.ToString()})";
+                doCalculate();
+            } else if( obj.ToString() == "PERC") {
+                Calculation = $"{_SBCalculation.ToString()}/100";
+                doCalculate(false);
+            }
+
+        }
+
+
+        public void doCalculate(Boolean reset = true)
+        {
             try
             {
-                DataTable dt = new DataTable();
-                var v = dt.Compute(Calculation, "");
+                // use nCalc lib                
+                var v = new NCalc.Expression(Calculation).Evaluate();
                 Calculation = v.ToString();
-            
-            } 
+
+                if (reset)
+                {
+                    _SBCalculation.Clear();
+                    reset = false;
+                } else
+                {
+                    _SBCalculation.Clear();
+                    _SBCalculation.Append(Calculation);
+                }
+
+            }
             catch (Exception ex)
             {
                 Calculation = "Error";
 
-            } finally
-            {
-                doReset = true;
             }
-            
+           
         }
-
         #region properties
 
         public string Calculation {
